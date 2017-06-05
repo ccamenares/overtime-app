0272c209795a8ea7d132d9bfac3c68401ab64a16
Christopher Camenares <camenarescoding@gmail.com>
2017-06-05 11:18:27 -0400

    Added Layout

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'navigate' do
+	before do 
+			user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
+			login_as(user, :scope => :user)
+	end
 	describe 'index' do 
 		it 'can be reached successfully' do 
 			visit posts_path
@@ -15,8 +19,6 @@ describe 'navigate' do
 
 	describe 'creation' do 
 		before do 
-			user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
-			login_as(user, :scope => :user)
 			visit new_post_path
 		end
 		it 'has a form that can be reached' do 
