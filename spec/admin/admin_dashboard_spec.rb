e3e798523245e461102001770030f9ce12665bf0
Christopher Camenares <camenarescoding@gmail.com>
2017-06-06 16:09:47 -0400

    Admin dashboard security check

---
+++ spec/admin/admin_dashboard_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "admin dashboard" do 
+	scenario "can be reached successfully" do 
+		visit admin_root_path
+		expect(page.status_code).to eq(200)
+	end
+
+	scenario "it does not allow users to access without being signed in" do 
+		visit admin_root_path
+		expect(page.current_path).to eq(new_user_session_path)
+	end
+
+	scenario "cannot be reached by a non-admin user" do 
+		user = FactoryGirl.create(:user)
+		login_as(user, :scope => :user)
+
+		visit admin_root_path
+
+		expect(current_path).to eq(root_path)
+	end
+end
+
+__END__
+
+For some reason it was necessary to use RSpec and not context/describe/it
