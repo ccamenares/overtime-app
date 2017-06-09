72e302a79fd92621dcbb448803dbc9bfb44820bf
Christopher Camenares <camenarescoding@gmail.com>
2017-06-09 15:19:05 -0400

    Created SMS stub for test

---
+++ lib/fake_sms.rb
@@ -0,0 +1,12 @@
+module FakeSMS
+	Message = Struct.new(:num, :msg)
+	@messages = []
+
+	def self.send_sms(num:, msg:)
+		@messages << Message.new(num, msg)
+	end
+
+	def self.messages
+		@messages
+	end
+end
