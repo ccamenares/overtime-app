a860e9177676158a069c8d7907ab4b59e31855fe
Christopher Camenares <camenarescoding@gmail.com>
2017-06-14 14:57:22 -0400

    Swapped out overtime request for daily hours in post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614154906) do
+ActiveRecord::Schema.define(version: 20170614184340) do
 
   create_table "audit_logs", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,11 +42,11 @@ ActiveRecord::Schema.define(version: 20170614154906) do
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "user_id"
-    t.integer  "status",           default: 0
-    t.decimal  "overtime_request", default: "0.0"
+    t.integer  "status",      default: 0
+    t.decimal  "daily_hours", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@
 
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 2.5)
+	Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, daily_hours: 8.5)
 end
 
 	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
